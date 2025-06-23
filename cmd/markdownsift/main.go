6ba5ff5d89b98b789b13ba418b2bfdc370919269
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2025-06-23 11:23:16 +0200

    Add CLI flag for source directory and file listing

--- cmd/markdownsift/main.go
+++ cmd/markdownsift/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,6 +9,19 @@ import (
 )
 
 func main() {
+	path := flag.String("path", "/Users/ralfwirdemann/Documents/zettelkasten", "source directory to parse")
+	flag.Parse()
+
+	files, err := markdownsift.ListFiles(*path, "^\\d{4}-\\d{2}-\\d{2}\\.md$")
+	if err != nil {
+		fmt.Printf("Error listing files: %v\n", err)
+		return
+	}
+
+	for _, file := range files {
+		fmt.Printf("Processing file: %s\n", file)
+	}
+
 	file, err := os.Open("/Users/ralfwirdemann/Documents/zettelkasten/2025-06-20.md")
 	if err != nil {
 		fmt.Printf("Error opening file: %v\n", err)
