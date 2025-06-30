e33cb50256d08a129ecf8ded45cd9bf38bc37fd3
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2025-06-30 22:09:42 +0200

    Add output flag and file output support

--- cmd/markdownsift/main.go
+++ cmd/markdownsift/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/rwirdemann/markdownsift"
@@ -11,8 +14,12 @@ import (
 func main() {
 	path := flag.String("path", "/Users/ralfwirdemann/Documents/zettelkasten", "source directory to parse")
 	tags := flag.String("tags", "", "comma separated list of tags to be parsed (hash sign omitted, default = empty is all)")
+	output := flag.String("output", "stdout", "output destination. values (stdout, file)")
+	outputDir := flag.String("output-dir", "/Users/ralfwirdemann/Documents/zettelkasten/topics", "output directory for file output")
+
 	flag.Parse()
-	if *path == "" {
+	if err := validateFlags(*path, *output, *outputDir); err != nil {
+		log.Println("Error:", err)
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -28,6 +35,43 @@ func main() {
 
 	snippets := markdownsift.Filter(markdownsift.CollectSnippets(*path), tt)
 	for tag, blocks := range snippets {
-		markdownsift.Write(os.Stdout, tag, blocks)
+		writeBlocks(tag, blocks, *output, *outputDir)
 	}
+}
+
+func validateFlags(path, output, outputDir string) error {
+	if path == "" {
+		return fmt.Errorf("path cannot be empty")
+	}
+	if output != "stdout" && output != "file" {
+		return fmt.Errorf("output must be either 'stdout' or 'file'")
+	}
+	if output == "file" && outputDir == "" {
+		return fmt.Errorf("output-dir is required when output is 'file'")
+	}
+	return nil
+}
+
+func writeBlocks(tag string, blocks []markdownsift.Block, output, outputDir string) {
+	writer := os.Stdout
+	if output == "file" {
+		// Create output directory if it doesn't exist
+		err := os.MkdirAll(outputDir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		path := filepath.Join(outputDir, tag[1:]+".md")
+		file, err := os.Create(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		writer = file
+		defer func() {
+			if err := file.Close(); err != nil {
+				log.Fatal(err)
+			}
+		}()
+	}
+	markdownsift.Write(writer, tag, blocks)
 }
